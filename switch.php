ab49359d94c0ac208972e3752b1b347b27e255f6
Ismail Ali <122273432+Ismail-webdev@users.noreply.github.com>
2025-01-02 15:11:49 +0530

    initial commit

--- switch.php
+++ switch.php
@@ -11,4 +11,15 @@ switch ($day) {
     default:
         echo "Midweek days.";
 }
+
+$status = 200;
+
+$message = match ($status) {
+    200 => 'OK',
+    404 => 'Not Found',
+    500 => 'Internal Server Error',
+    default => 'Unknown Status',
+};
+
+echo $message; // Output: OK
 ?>
