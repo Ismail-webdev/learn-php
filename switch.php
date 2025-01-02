66ac1db04ec7f45c730540dc42594bcff5f163fa
Ismail Ali <122273432+Ismail-webdev@users.noreply.github.com>
2025-01-02 15:04:50 +0530

    intial commit

---
+++ switch.php
@@ -0,0 +1,14 @@
+<?php
+$day = "Monday";
+
+switch ($day) {
+    case "Monday":
+        echo "Start of the work week.";
+        break;
+    case "Friday":
+        echo "End of the work week.";
+        break;
+    default:
+        echo "Midweek days.";
+}
+?>
