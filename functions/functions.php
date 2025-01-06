a26bf5d32b68289ee4a14b096c01383b5ce377e8
Ismail <ismailali07863@gmail.com>
2025-01-06 09:21:45 +0530

    initial commit

--- functions/functions.php
+++ functions/functions.php
@@ -31,4 +31,49 @@ echo additon(5, 10);
 // echo additon(5.5, 10.5);
 
 // Variadic Functions
+function sum(int ...$numbers):int {
+    var_dump($numbers);
+    $sum = 0;
+    foreach($numbers as $number){
+        $sum += $number;
+    }
+    return $sum;
+}
+
+var_dump(sum());
+var_dump(sum(6));
+var_dump(sum(6,1,3));
+
+function introduceTeam(string $team,string ...$members):void{
+    echo "Team $team:\n";
+    var_dump($members);
+    echo "Members: ".implode(", ", $members)."\n";
+}
+
+introduceTeam("A","Luffy","Naurto","Goku");
+
+// unpacking arrays into arguments
+$members =[ "A","B","C"];
+introduceTeam("X",...$members);
+
+$greet = function(string $name):string{
+    return "Hello, $name!";
+};
+echo  $greet("Robert");
+
+$numbers = [1,3,5];
+$squared = array_map(function(int $n):int{
+    return $n * $n;
+},$numbers);
+
+
+var_dump($squared);
+
+$outer = 5;
+$fun = function() use ($outer){
+    $outer++;
+    return $outer;
+};
+
+echo $fun();
 ?>
